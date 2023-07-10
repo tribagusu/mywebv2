88adbff7ed2857999e3ad3804445bb24fb7b0e13
tribagus <tribagus.indonesia@gmail.com>
2023-07-10 11:17:29 +0700

    style: change border color on navbar and footer for dark mode

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="flex px-4 justify-center w-11/12 md:w-8/12 mx-auto border-t border-dark">
+    <footer className="flex px-4 justify-center w-11/12 md:w-8/12 mx-auto border-t border-dark dark:border-darkLight">
       <span className=" dark:text-light text-center text-sm font-thin py-6 md:py-8 ">
         &copy; 2023 <strong className="dark:text-white">triBagus</strong>{" "}
         powered by &hearts;
